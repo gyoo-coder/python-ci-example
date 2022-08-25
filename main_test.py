69ec2418bdb81637bb88c2ae94e8788866e04bbb
gyoo-coder <gyoogler.lee@gmail.com>
2022-08-25 10:03:54 +0900

    add test code

---
+++ main_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+from main import hello
+
+class MainTest(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello World!")
+
+if __name__=="__main__":
+    unittest.main()
